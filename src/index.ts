371013812c09288c26b4048e01064e40e8b94b2c
brendan <brendangooch@gmail.com>
2024-06-08 01:24:55 +0100

    added barrel file to package to expose screen object + composite + view object

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * package barrel file
+ */
+
+
+
+export { ScreenObject } from './screen-object.js';
+export { ScreenObjectComposite } from './screen-object-composite.js';
+export { ScreenObjectView } from './screen-object-view.js';
+export { ScreenObjectCompositeView } from './screen-object-composite-view.js';
